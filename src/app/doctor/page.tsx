08761cb577b3f608f4cda0a164467ca91e54d302
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-11-14 17:54:02 +0530

    initial ui

---
+++ src/app/doctor/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+
+const page = () => {
+	return (
+		<div className="bg-neutral-800 flex flex-row text-white h-screen">
+			<div className="w-1/4 bg-gradient-to-br from-green-200 to-green-300 ">
+				<div className="ml-4 mt-4 ">
+					<Image
+						src="/samplehero.png"
+						height={200}
+						width={200}
+						alt="User Profile Picture"
+						className="rounded-full border-[1px] border-green-900"
+					></Image>
+				</div>
+				<div className="ml-6 mt-4 text-2xl text-green-950 font-semibold ">
+					Dr. Samkit Samsukha
+				</div>
+				<div className="rounded-full p-2 bg-gradient-to-br from-blue-200 to-blue-300 w-fit ml-6 mt-2 px-6 border-[1px] border-green-950 text-black font-semibold">Endicronologist</div>
+			</div>
+			<div className="px-16 py-8 w-3/4 flex flex-col space-y-4">
+				<div className="text-3xl font-semibold">Hello Doctor Name,</div>
+				
+			</div>
+		</div>
+	);
+};
+
+export default page;
--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -1,11 +1,87 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { FaSuitcaseMedical } from "react-icons/fa6";
+import { MdOutlineSecurity } from "react-icons/md";
+import { FaStethoscope } from "react-icons/fa";
+import { RiMoneyRupeeCircleFill } from "react-icons/ri";
 
 const page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<div className="bg-neutral-800 flex flex-row text-white h-screen">
+			<div className="w-1/4 bg-gradient-to-br from-blue-200 to-blue-300 ">
+				<div className="ml-4 mt-4 ">
+					<Image
+						src="/samplehero.png"
+						height={200}
+						width={200}
+						alt="User Profile Picture"
+						className="rounded-full border-[1px] border-blue-900"
+					></Image>
+				</div>
+				<div className="ml-6 mt-4 text-2xl text-blue-950 font-semibold ">
+					Samkit Samsukha
+				</div>
+				<div className="ml-6 mt-4 text-black font-semibold">Gender - Male</div>
+				<div className="ml-6 text-black font-semibold">DOB - 14/10/2005</div>
+				<div className="ml-6 mt-4 bg-green-100 w-3/4 p-1 rounded-md text-black font-semibold">
+					Phone No. - 9239089089
+				</div>
+				<div className="ml-6 mt-2 text-black bg-green-100 p-1 w-3/4 rounded-md font-semibold">
+					Email - samkitsamsukha@gmail.com
+				</div>
+				<div className="ml-6 mt-4 grid grid-cols-2 gap-1 w-3/4 text-black text-center">
+  <div className="p-4 bg-blue-100">
+    <span className="font-semibold text-xl">19 years</span> <br /> 
+    <span className="text-neutral-800">Age</span>
+  </div>
+  <div className="p-4 bg-green-100">
+    <span className="font-semibold text-xl">A+</span> <br /> 
+    <span className="text-neutral-800">Blood Group</span>
+  </div>
+  <div className="p-4 bg-green-100">
+    <span className="font-semibold text-xl">77 kg</span> <br /> 
+    <span className="text-neutral-800">Weight</span>
+  </div>
+  <div className="p-4 bg-blue-100">
+    <span className="font-semibold text-xl">181 cm</span> <br /> 
+    <span className="text-neutral-800">Height</span>
+  </div>
+</div>
+			</div>
+			<div className="px-16 py-8 w-3/4 flex flex-col space-y-4">
+				<div className="text-3xl font-semibold">Hello Patient Name,</div>
+				<div>
+					At Vitalis, we prioritize providing top-notch facilities for our
+					patients. We are committed to ensuring their comfort and well-being.
+					Additionally, we implement robust measures to safeguard patient data,
+					maintaining confidentiality and security at all times. Your trust is
+					our utmost priority. At Vitalis, we prioritize providing top-notch
+					facilities for our patients. We are committed to ensuring their
+					comfort and well-being. Additionally, we implement robust measures to
+					safeguard patient data, maintaining confidentiality and security at
+					all times. Your trust is our utmost priority.{" "}
+				</div>
+				<div className="flex md:flex-row flex-col md:space-x-4 justify-center">
+					<button className="text-black p-4 w-1/3 text-xl flex flex-col items-center justify-center hover:from-blue-300 hover:to-blue-400 duration-300 transition-all border-blue-900 border-[1px] rounded-md bg-gradient-to-br from-blue-200 to bg-blue-300">
+						<FaSuitcaseMedical />
+						<div className="pt-3">Access Medical Records</div>
+					</button>
+					<button className="text-black p-4 w-1/3 text-xl flex flex-col items-center justify-center hover:from-green-300 hover:to-green-400 duration-300 transition-all border-green-900 border-[1px] rounded-md bg-gradient-to-br from-green-200 to bg-green-300">
+						<MdOutlineSecurity />
+						<div className="pt-3">Get insurance</div>
+					</button>
+					<button className="text-black p-4 w-1/3 text-xl flex flex-col items-center justify-center hover:from-blue-300 hover:to-blue-400 duration-300 transition-all border-blue-900 border-[1px] rounded-md bg-gradient-to-br from-blue-200 to bg-blue-300">
+						<FaStethoscope />
+						<div className="pt-3">Consult a doctor</div>
+					</button>
+					<button className="text-black p-4 w-1/3 text-xl flex flex-col items-center justify-center hover:from-green-300 hover:to-green-400 duration-300 transition-all border-green-900 border-[1px] rounded-md bg-gradient-to-br from-green-200 to bg-green-300">
+						<RiMoneyRupeeCircleFill />
+						<div className="pt-3">Sell your data</div>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default page
+export default page;
