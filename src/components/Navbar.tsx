e45010e3b523e1b067a079c85cbcafeab25b826e
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-11-15 15:18:18 +0530

    logout singin signup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,37 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { checkToken } from "@/lib/actions/jwtLogics";
+import { toast } from "sonner";
 const Navbar = () => {
     const router = useRouter();
+    const [isLogged, setIsLogged] = useState(false);
+
+    async function loggedCheck() {
+        const verify = await checkToken(localStorage.getItem("token") || "");
+        if (verify) {
+            setIsLogged(true);
+        }
+    }
+
+    useEffect(() => {
+        loggedCheck();
+    }, []);
+
+    function logOut(){
+        localStorage.setItem('token',"");
+        router.push('/');
+        toast.success("Logged out successfully !!")
+        setIsLogged(false)
+    }
+
     return (
         <div className="w-full flex flex-row shadow-md justify-between bg-white">
-            <div onClick={()=>router.push('/')} className="flex flex-row cursor-pointer justify-center items-center p-2 ml-6 text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-teal-600">
+            <div
+                onClick={() => router.push("/")}
+                className="flex flex-row cursor-pointer justify-center items-center p-2 ml-6 text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-teal-600"
+            >
                 <Image
                     src={"/logo.png"}
                     height={50}
@@ -15,10 +40,31 @@ const Navbar = () => {
                 ></Image>
                 VITALIS
             </div>
-            <div className="mr-6 p-2 flex flex-row justify-center items-center space-x-2 w-[180px]">
-              <button onClick={()=>router.push('/onboarding')} className="text-black w-1/2  px-2 py-1 rounded-md hover:bg-gray-200 border-[1px] border-teal-800 duration-300 transition-all">Sign Up</button>
-              <button onClick={()=>router.push('/login')} className="text-white w-1/2 bg-teal-700 px-2 py-1 rounded-md hover:bg-teal-800 duration-300 transition-all">Log In</button>
-            </div>
+            {isLogged ? (
+                <div className="mr-6 p-2 flex flex-row justify-center items-center space-x-2 w-[180px]">
+                    <button
+                        onClick={logOut}
+                        className="text-white w-1/2 bg-teal-700 px-2 py-1 rounded-md hover:bg-teal-800 duration-300 transition-all"
+                    >
+                        Log out
+                    </button>
+                </div>
+            ) : (
+                <div className="mr-6 p-2 flex flex-row justify-center items-center space-x-2 w-[180px]">
+                    <button
+                        onClick={() => router.push("/onboarding")}
+                        className="text-black w-1/2  px-2 py-1 rounded-md hover:bg-gray-200 border-[1px] border-teal-800 duration-300 transition-all"
+                    >
+                        Sign Up
+                    </button>
+                    <button
+                        onClick={() => router.push("/login")}
+                        className="text-white w-1/2 bg-teal-700 px-2 py-1 rounded-md hover:bg-teal-800 duration-300 transition-all"
+                    >
+                        Log In
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
